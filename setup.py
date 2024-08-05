1a4e210452612da9b946b0361aa633886f012cbf
Artem Mikhaylov <me@utb404.ru>
2024-08-05 17:57:15 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='utb_test_package',
-    version='0.1.0',
+    version='0.2.0',
     author='Artem Mikhaylov',
     author_email='utb404@gmail.com',
     description='Краткое описание вашего пакета',
